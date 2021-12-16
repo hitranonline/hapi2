cc4137c41401fe59fb72c877615d637843c3f0fd
Raymond Menzel <Raymond.Menzel@noaa.gov>
2021-12-16 16:34:06 -0500

    remove unnecessary(?) files

--- build/lib/hapi2/collect/__init__.py
+++
@@ -1,1 +0,0 @@
-from .jeanny3 import *
--- build/lib/hapi2/db/sqlalchemy/sqlite/updaters.py
+++
@@ -1,7 +0,0 @@
-from . import models
-
-from hapi2.db.sqlalchemy import Updater
-
-class Updater(updaters.Updater):    
-    def __init__(self):
-        self.models = models
